7f8305ce1b76a4516b83f939d7cc3e54de4b0999
Dinakaran <dinadinakaran143@gmail.com>
2023-08-26 08:25:37 +0530

    new note update future added and network call added

--- src/component/Note.tsx
+++ src/component/Note.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {RiDeleteBin4Fill} from "react-icons/ri"
-import { Prop, deleteNote } from '../Redux/reducer/NotesReducer';
+import { Prop, deleteNote, updateNote } from '../Redux/reducer/NotesReducer';
 import { useAppdispatch } from './Uitils/Hooks';
 import netWorkCall from './Uitils/Network';
 
@@ -9,7 +9,10 @@ interface note {
 }
 
 const Note : React.FC<note> = ({note}:note) => {
- const dispatch = useAppdispatch()
+  const [IsEdit,setIsEdit]=useState(false);
+  const[text,setText]=useState(note)
+ const dispatch = useAppdispatch();
+ let chaaractorlength=200
     const handleDelete =async(id:number)=>{
            
             let localy_stroed :any = localStorage.getItem('user');
@@ -25,14 +28,45 @@ const Note : React.FC<note> = ({note}:note) => {
              
             }
     }
+    const handlesubmit =async(e:any)=>{
+      setIsEdit(false)
+   
+      let localy_stroed :any = localStorage.getItem('user');
+      if(localy_stroed){
+        dispatch(updateNote(text));
+        let user= JSON.parse(localy_stroed);
+        let result = await netWorkCall("POST","/user/updatenote",{email:user.email,data:text});
+       // console.log(result)
+        if(!result.isOk){
+         return alert(result.message)
+        }
+       // alert(result.message)
+       
+      }
+       console.log(text)
+    }
+    const handlechanges = (ele:any)=>{
+      setText({...text,[ele.target.name]:ele.target.value})
+  }
   return (
     <div className='note'>
-      <span>{note.content}</span>
+     { IsEdit?  <textarea className='text-area' onClick={()=>{setIsEdit(true)}} value={text.content} name="content" onChange={(e:any)=>{handlechanges(e)}} >{text.content}</textarea> :  <span onClick={()=>{setIsEdit((pre)=>!pre)}} >{note.content}</span>
+
+     }
+     {
+      IsEdit ? 
+      
       <div className="note__footer">
+      <small>{chaaractorlength - text.content.length} Remaining</small>
+      <button onClick={handlesubmit} className='btn'>Save</button>
+
+    </div> : <div className="note__footer">
             <small>{note.date}</small>
             <RiDeleteBin4Fill className='delete__icon' onClick={()=>{handleDelete(note.id)}}  />
       </div>
+}
     </div>
+
   )
 }
 
